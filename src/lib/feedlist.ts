31a7f73b1f04b03f41be51f7d396255140ec316e
gallynaut <Connerngallagher@gmail.com>
2021-10-01 15:25:09 -0600

    new

--- src/lib/feedlist.ts
+++ src/lib/feedlist.ts
@@ -1,7 +1,6 @@
 import { fetch } from 'cross-fetch';
 
 import feedlist from '../feeds/feedlist.json';
-import { FeedInfo, FeedList } from '../types';
 
 export type FeedInfoMap = Map<string, FeedInfo>;
 
@@ -160,4 +159,54 @@ export class FeedListContainer {
   getList = () => {
     return this.feedList;
   };
+}
+
+export interface FeedList {
+  readonly name: string;
+  readonly description: string;
+  readonly tags: { [tag: string]: TagDetails };
+  readonly timestamp: string;
+  readonly feeds: FeedInfo[];
+}
+
+export interface TagDetails {
+  readonly name: string;
+  readonly description: string;
+}
+
+// export const enum CurrencyPairType {
+//   Fiat = 'Fiat',
+//   Solana = 'Solana',
+//   Stablecoin = 'Stablecoin',
+//   SplToken = 'SplToken',
+//   WrappedToken = 'WrappedToken',
+//   NFT = 'NFT',
+//   Other = 'Other',
+// }
+
+// export type CurrencyPairType =
+// | 'Fiat'
+// | 'Solana'
+// | 'Stablecoin'
+// | 'SplToken'
+// | 'WrappedToken'
+// | 'NFT'
+// | 'Other';
+
+export interface CurrencyPair {
+  readonly type: string;
+  readonly name: string;
+  readonly symbol: string;
+  readonly mintAddress?: string;
+}
+
+export interface FeedInfo {
+  readonly name: string;
+  readonly description?: string;
+  readonly chainId: number;
+  readonly feedAddress: string;
+  readonly optimizedFeedAddress: string;
+  readonly quotePair?: CurrencyPair;
+  readonly basePair?: CurrencyPair;
+  readonly tags?: string[];
 }
